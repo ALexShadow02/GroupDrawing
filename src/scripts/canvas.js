33a6e1d16618b140e973f07512a38a2050edc2dd
alhorist02 <alhorist@gmail.com>
2021-04-25 11:58:18 +0300

    A lot of key changes

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -1,4 +1,5 @@
 let mouseDown = false
+let draggingFigure = null
 window.onkeypress = (event) => {
     let keyCode = event.code.slice(event.code.length-1, event.code.length)
     if(keyCode == 'S') {
@@ -42,92 +43,33 @@ window.onkeypress = (event) => {
 const points = []
 const figures = []
 const canv = document.getElementById('canvas')
+const canvas = new fabric.Canvas('canvas')
 const canvPos = canv.getBoundingClientRect()
-canv.onmouseover = () => {
-    canv.style.border = '3px black dashed'
+/*canv.onmousedown = customMouseDown
+canv.onmousemove = customMouseMove
+canv.onmouseup = customMouseUp*/
+document.getElementById('clearBtn').onclick = () => {
+    ctx.clearRect(0, 0, canv.width, canv.height)
+    figures.length = 0 
 }
-canv.onmouseout = () => {
-    canv.style.border = '5px black solid'
-}
-canv.onmousedown = (event) => {
+const ctx = canv.getContext('2d')
+function customMouseDown(event){
     let x0 = event.clientX - canvPos.left
     let y0 = event.clientY - canvPos.top
     let mode = document.forms[0].mode.value
-    if(mode == 'fill' || mode == 'move') return
-    console.log('Figure drawing started')
+    if(mode == 'fill') return
     mouseDown = true
     points.push([x0, y0])
+    if(mode == 'move'){
+        draggingFigure = locateFigure([x0, y0])
+        return
+    }
+    console.log('Figure drawing started')
     if(mode != 'pencil' && mode != 'line') return
     ctx.beginPath()
     ctx.moveTo(x0, y0)
 }
-canv.onmouseup = (event) => {
-    mouseDown = false
-    let mode = document.forms[0].mode.value
-    let x1 = event.clientX - canvPos.left
-    let y1 = event.clientY - canvPos.top
-    if(mode == 'rect'){
-        let [x0, y0] = points[0]
-        ctx.clearRect(x0, y0, Math.floor(points[1][0]-x0), Math.floor(points[1][1]-y0))
-        points.pop()
-        if(x0 < x1 && y0 < y1){
-            ctx.strokeRect(x0, y0, Math.floor(x1-x0), Math.floor(y1-y0))
-            setTimeout(() => {
-                ctx.fillRect(x0, y0, Math.floor(x1-x0), Math.floor(y1-y0))
-            }, 300)
-        }
-        else {
-            alert('Not possible to draw a rect')
-            points.length = 0 
-            return
-        }
-    }
-    else if(mode == 'circle'){
-        ctx.beginPath()
-        ctx.arc(...points[0], Math.abs(Math.floor(x1-points[0][0])), 0, Math.PI * 2)
-        ctx.closePath()
-        ctx.stroke()
-        setTimeout(() => {
-            ctx.fill()
-        }, 300)
-    }
-    else if(mode == 'pencil' || mode == 'line'){
-        ctx.lineTo(x1, y1)
-        ctx.closePath()
-        ctx.stroke()
-    }
-    else if(mode == 'move'){
-        locateFigure([x1, y1])
-        return
-    }
-    else {
-        let locatedFigure = locateFigure([x1, y1])
-        fillFigure(locatedFigure)
-        return
-    }
-    /*for(let i = 0; i < points.length;i++){
-        points[i][0] += 150
-    } 
-    drawByPoints(points)*/
-    points.push([x1, y1])
-    let figure = {}
-    figure.type = mode
-    figure.strokeStyle = ctx.strokeStyle
-    if(figure.type != 'circle') figure.points = clone(points)
-    if(mode == 'rect') {
-        figure.width = Math.floor(x1-points[0][0])
-        figure.height = Math.floor(y1-points[0][1])
-        figure.fillStyle = ctx.fillStyle
-    } 
-    if(mode == 'circle'){
-        figure.centre = points[0]
-        figure.radius = Math.abs(Math.floor(x1-points[0][0]))
-        figure.fillStyle = ctx.fillStyle
-    }
-    figures.push(figure)
-    points.length = 0 
-}
-canv.onmousemove = (event) => {
+function customMouseMove(event){
     let x1 = event.clientX - canvPos.left
     let y1 = event.clientY - canvPos.top
     let mode = document.forms[0].elements[0].value
@@ -148,12 +90,110 @@ canv.onmousemove = (event) => {
         points.push([x1, y1])
         ctx.strokeRect(x0, y0, Math.floor(x1-x0), Math.floor(y1-y0))
     }
+    else if(mouseDown && mode == 'move'){
+        let [x0, y0] = points[0]
+        let dx = x1 - x0
+        let dy = y1 - y0
+        if(draggingFigure.type == 'rect'){
+            draggingFigure.points[0][0] += dx
+            draggingFigure.points[0][1] += dy
+        }
+        else if(draggingFigure.type == 'circle'){
+            draggingFigure.centre[0] += dx
+            draggingFigure.centre[1] += dy
+        }
+        ctx.clearRect(0, 0, canv.width, canv.height)
+        drawFigures(figures)
+        points[0] = [x1, y1]
+    }
 }
-document.getElementById('clearBtn').onclick = () => {
-    ctx.clearRect(0, 0, canv.width, canv.height)
-    figures.length = 0 
+function customMouseUp(event){
+    mouseDown = false
+    let mode = document.forms[0].mode.value
+    let x1 = event.clientX - canvPos.left
+    let y1 = event.clientY - canvPos.top
+    if(mode == 'rect'){
+        let [x0, y0] = points[0]
+        ctx.clearRect(x0, y0, Math.floor(points[1][0]-x0), Math.floor(points[1][1]-y0))
+        points.pop()
+        if(x0 < x1 && y0 < y1){
+            ctx.strokeRect(x0, y0, Math.floor(x1-x0), Math.floor(y1-y0))
+            setTimeout(() => {
+                ctx.fillRect(x0, y0, Math.floor(x1-x0), Math.floor(y1-y0))
+            }, 300)
+        }
+        else {
+            alert('Not possible to draw a rect')
+            points.length = 0 
+            return
+        }
+    }
+    else if(mode == 'triangle'){
+        let [x0, y0] = points[0]
+        if(y0 < y1){
+            ctx.beginPath()
+            let width = Math.abs(x1 - x0) * 2
+            ctx.moveTo(x0, y0)
+            ctx.lineTo(x1, y1)
+            ctx.lineTo(x1 + width, y1)
+            ctx.closePath()
+            ctx.fill()
+            ctx.stroke()
+        }
+        else {
+            alert('Not possible to draw a triangle')
+            points.length = 0 
+            return
+        }
+    }
+    else if(mode == 'circle'){
+        ctx.beginPath()
+        ctx.arc(...points[0], Math.abs(Math.floor(x1-points[0][0])), 0, Math.PI * 2)
+        ctx.closePath()
+        ctx.stroke()
+        setTimeout(() => {
+            ctx.fill()
+        }, 300)
+    }
+    else if(mode == 'pencil' || mode == 'line'){
+        ctx.lineTo(x1, y1)
+        ctx.closePath()
+        ctx.stroke()
+    }
+    else if(mode == 'move'){
+        points.length = 0 
+        draggingFigure = null
+        return
+    }
+    else {
+        let locatedFigure = locateFigure([x1, y1])
+        fillFigure(locatedFigure)
+        return
+    }
+    points.push([x1, y1])
+    let figure = {}
+    figure.type = mode
+    figure.strokeStyle = ctx.strokeStyle
+    if(figure.type != 'circle' && figure.type != 'triangle') figure.points = clone(points)
+    if(mode == 'rect') {
+        figure.width = Math.floor(x1-points[0][0])
+        figure.height = Math.floor(y1-points[0][1])
+        figure.fillStyle = ctx.fillStyle
+    } 
+    else if(mode == 'circle'){
+        figure.centre = points[0]
+        figure.radius = Math.abs(Math.floor(x1-points[0][0]))
+        figure.fillStyle = ctx.fillStyle
+    }
+    else if(mode == 'triangle'){
+        figure.topPoint = points[0]
+        figure.width = Math.abs(x1 - points[0][0]) * 2
+        figure.height = Math.abs(y1 - points[0][1])
+        figure.fillStyle = ctx.fillStyle
+    }
+    figures.push(figure)
+    points.length = 0 
 }
-const ctx = canv.getContext('2d')
 function drawByPoints(points){
     for(let i = 0; i < points.length-1; i++){
         ctx.beginPath()
@@ -171,6 +211,18 @@ function drawFigures(figures){
             ctx.strokeStyle = figure.strokeStyle
             ctx.strokeRect(x0, y0, figure.width, figure.height)
             ctx.fillRect(x0, y0, figure.width, figure.height)
+        }
+        else if(figure.type == 'triangle'){
+            ctx.beginPath()
+            let [x0, y0] = figure.topPoint
+            let x1 = x0 - (figure.width / 2)
+            let y1 = y0 + figure.height
+            ctx.moveTo(x0, y0)
+            ctx.lineTo(x1, y1)
+            ctx.lineTo(x1 + figure.width, y1)
+            ctx.closePath()
+            ctx.fill()
+            ctx.stroke()
         }
         else if(figure.type == 'circle'){
             ctx.beginPath()
